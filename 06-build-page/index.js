7d8e86cf8bcdffac801308415f55dd75edecb3e1
Aliaksandr Konik <cibulyadev@gmail.com>
2022-11-10 21:08:32 +0100

    commit

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -18,32 +18,20 @@ const writeIndexHtml = async() => {
     const pathToIndex = path.join(pathToDist, 'index.html');
 
     const readAndReplace = async () => {
-      const readTempl = await fs.promises.readFile(htmlTempl, { encoding: 'utf-8' });
-
-      const writeIndex = await fs.promises.writeFile(pathToIndex, readTempl); //create index.html
-      const indexInner = await fs.promises.readFile(pathToIndex, { encoding: 'utf-8' })
-      const readComps = await fs.promises.readdir(compPath, { encoding:'utf-8' })
-      readComps.forEach(async file => {
-        let filePath = path.join(compPath, `${file}`);
-        let fileName = path.basename(`${file}`, '.html');
+      const readComps = await fs.promises.readdir(compPath, { encoding: 'utf-8' });
+      for (let i = 0; i < readComps.length; i++){
+        let filePath = path.join(compPath, `${readComps[i]}`);
+        let fileName = path.basename(`${readComps[i]}`, '.html');
         let fileInner = (await fs.promises.readFile(filePath, { encoding: 'utf-8' })).toString();
-        // console.log(fileInner + '\n')
-        let finalData = (await fs.promises.readFile(pathToIndex, { encoding: 'utf-8' }, 'index.html')).toString();
-        // console.log(fileInner)
-        let a =  finalData.replace(`{{${fileName}}}`, fileInner.toString())
-      
-        // const finalHtml= await  fs.promises.writeFile(path.join(pathToDist,'index.html'),finalData)
-        // console.log(pathToIndex)
-        fs.promises.writeFile(pathToIndex, a)
-      })
-      
-      
- 
-
-    
-    
+        const readTempl = await fs.promises.readFile(htmlTempl, { encoding: 'utf-8' });
+        // const writeIndex = await fs.promises.writeFile(pathToIndex, readTempl); //create index.html
+        const indexInner = await fs.promises.readFile(pathToIndex, { encoding: 'utf-8' });
+        const newIndex = indexInner.replace(`{{${fileName}}}`, fileInner);
+        let fileIndex = await fs.promises.writeFile(path.join(pathToDist, 'index.html'), newIndex);
+        i++;
+      }
     }
-    readAndReplace()
+    readAndReplace();
   }
 
   catch(err){}
@@ -55,7 +43,37 @@ writeIndexHtml();
 
 
 
+const stylesLocation = path.join(__dirname, 'styles');
 
+const pathTobundleCss = path.join(__dirname, 'project-dist', 'style.css');
+
+const writebleStream = fs.createWriteStream(pathTobundleCss);
+
+const filesList = fs.promises.readdir(stylesLocation);
+
+filesList
+  .then(async data => {
+
+    for (let i = 0; i < data.length; i++) {
+
+      const files = await fs.promises.readdir(stylesLocation);
+
+      const filesPaths = path.join(stylesLocation, files[i]);
+
+      // console.log(files[i])
+
+      if (files[i].includes('css')) {
+
+        const readStr = fs.createReadStream(path.join(stylesLocation, path.basename(filesPaths)));
+
+        readStr.on('data', data => {
+
+          writebleStream.write(data.toString() + '\n')
+        })
+      }
+    }
+  })
+  .catch(err => console.log(err))
 
 
 
